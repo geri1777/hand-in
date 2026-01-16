90a63c7f6fb432c61fe5edb602a9fdeafa49c0f4
Geri76 <geri@flacker.net>
2026-01-16 12:15:46 +0100

    Adds support for sixth uploaded cat image
    Extends the set of already uploaded images to include an additional cat image, enhancing available assets for further usage or display.

--- src/file_helper.ts
+++ src/file_helper.ts
@@ -4,6 +4,7 @@ import cat2WebpUrl from "./static/already_uploaded/cat2.webp";
 import cat3WebpUrl from "./static/already_uploaded/cat3.webp";
 import cat4WebpUrl from "./static/already_uploaded/cat4.webp";
 import cat5WebpUrl from "./static/already_uploaded/cat5.webp";
+import cat6WebpUrl from "./static/already_uploaded/cat6.webp";
 
 import icon1024Url from "./static/icons/icon1024.webp";
 import icon512Url from "./static/icons/icon512.webp";
@@ -15,6 +16,6 @@ import githubIconUrl from "./static/icons/github.svg";
 import arrowIconUrl from "./static/icons/arrow.svg";
 
 export const file_helper = {
-  already_uploaded: [catWebpUrl, cat2WebpUrl, cat3WebpUrl, cat4WebpUrl, cat5WebpUrl],
+  already_uploaded: [catWebpUrl, cat2WebpUrl, cat3WebpUrl, cat4WebpUrl, cat5WebpUrl, cat6WebpUrl],
   icons: { icon1024Url, icon512Url, icon256Url, icon128Url, icon64Url, githubIconUrl, arrowIconUrl },
 };
